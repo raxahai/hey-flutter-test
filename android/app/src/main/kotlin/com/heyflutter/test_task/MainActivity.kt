535d63aadd55fea33608dc08159700ca12d699b0
Syed Raza Haider <raxa.hai@gmail.com>
2024-11-19 16:27:57 +0500

    Add android google api-key and platform code
    - Set meta data in androidmanifest.xml
    - Update the google maps api_key via dynamic platform call

--- android/app/src/main/kotlin/com/heyflutter/test_task/MainActivity.kt
+++ android/app/src/main/kotlin/com/heyflutter/test_task/MainActivity.kt
@@ -1,5 +1,43 @@
 package com.heyflutter.test_task
 
+import android.content.pm.PackageManager
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+    private val CHANNEL = "com.heyflutter.test_task/meta"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                if (call.method == "setMetadata") {
+                    setMetadata(call, result)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+
+    private fun setMetadata(call: MethodCall, result: MethodChannel.Result){
+        val key = call.argument<String>("key")
+        val value = call.argument<String>("value")
+        try {
+            val appInfo = packageManager.getApplicationInfo(
+                packageName,
+                PackageManager.GET_META_DATA
+            )
+            val metaData = appInfo.metaData ?: Bundle()
+            metaData.putString(key, value)
+            appInfo.metaData = metaData
+
+            result.success("Metadata set successfully")
+        } catch (e: Exception) {
+            result.error("ERROR", "Failed to set metadata", null)
+        }
+    }
+}
