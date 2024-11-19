a84388b761f6772689cf538d15cb609cbc7b183f
Syed Raza Haider <raxa.hai@gmail.com>
2024-11-19 16:28:48 +0500

    Add google maps api_key iOS
    - Set meta data to info.plist
    - Dynamically change key via user input using platform call

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,39 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        let controller = window?.rootViewController as! FlutterViewController
+        let metaChannel = FlutterMethodChannel(name: "com.heyflutter.test_task/meta",
+                                               binaryMessenger: controller.binaryMessenger)
+        metaChannel.setMethodCallHandler { (call, result) in
+            if call.method == "setMetadata" {
+                guard let args = call.arguments as? [String: Any],
+                      let key = args["key"] as? String,
+                      let value = args["value"] as? String else {
+                    result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid arguments", details: nil))
+                    return
+                }
+                let plistPath = Bundle.main.path(forResource: "Info", ofType: "plist")
+                if let plistPath = plistPath, let plistDict = NSMutableDictionary(contentsOfFile: plistPath) {
+                    plistDict[key] = value
+                    plistDict.write(toFile: plistPath, atomically: true)
+                    GMSServices.provideAPIKey(value) // Google Maps API-KEY
+                    result("Metadata set successfully")
+                } else {
+                    result(FlutterError(code: "ERROR", message: "Failed to update Info.plist", details: nil))
+                }
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
